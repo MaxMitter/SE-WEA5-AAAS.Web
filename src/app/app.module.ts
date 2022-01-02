2826e96b1a4dedb13051c5049a25d2dcf6b8f264
Max Mitter <max.mitter14@gmail.com>
2022-01-02 11:37:16 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome'
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashComponent } from './dash/dash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(fasLib: FaIconLibrary) {
+    fasLib.addIconPacks(fas, far);
+  }
+}
